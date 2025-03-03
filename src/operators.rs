93496d0ff9feb92118c0b942b9693535a8bd803b
lss <jwliang2023.@163.com>
2025-03-03 21:05:29 +0800

    silu

--- src/operators.rs
+++ src/operators.rs
@@ -82,8 +82,14 @@ pub fn swiglu(y: &mut Tensor<f32>, x: &Tensor<f32>) {
 
     // let _y = unsafe { y.data_mut() };
     // let _x = x.data();
-
-    todo!("实现 silu，这里给了一些前期准备工作的提示，你可以参考")
+    let len = y.size();
+    assert!(len == x.size());
+    let mut y_date = unsafe { y.data_mut() };
+    let x_date = x.data();
+    for i in 0..len {
+        y_date[i] = y_date[i] * (x_date[i] / (1. + (-x_date[i]).exp()));
+    }
+    
 }
 
 // C = beta * C + alpha * A @ B^T
